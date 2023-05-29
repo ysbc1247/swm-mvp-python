0718fa1015415c3e9f6d77eeea3858289edf26f7
ysbc1247 <ystc1247@khu.ac.kr>
2023-05-30 04:08:07 +0900

    [FEAT] implemented basic get function for youtube transcript

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask
-import numpy
+from flask import Flask, jsonify
+from youtube_transcript_api import YouTubeTranscriptApi
+
 app = Flask(__name__)
 
+@app.route('/transcripts/<video_id>', methods=['GET'])
+def get_transcripts(video_id):
+    transcript_list = YouTubeTranscriptApi.list_transcripts(video_id)
+    transcript = transcript_list.find_transcript(['ko'])
+    return jsonify(transcript.fetch())
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
-
-
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(port=5000)
